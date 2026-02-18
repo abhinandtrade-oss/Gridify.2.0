d41fd6bc541e7693537ffced33b33bb4a1932057
abhinandtrade-oss <abhinand.trade@gmail.com>
2026-02-18 16:34:01 +0530

    Delete E-com directory

--- E-com/admin/sql/add_keywords_column.sql
+++
@@ -1,7 +0,0 @@
--- Add keywords column to products table if it doesn't exist
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'products' AND column_name = 'keywords') THEN
-        ALTER TABLE products ADD COLUMN keywords TEXT;
-    END IF;
-END $$;
--- E-com/admin/sql/add_previous_data_column.sql
+++
@@ -1,7 +0,0 @@
--- Add previous_data column to products table for storing version history
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'products' AND column_name = 'previous_data') THEN
-        ALTER TABLE products ADD COLUMN previous_data JSONB;
-    END IF;
-END $$;
